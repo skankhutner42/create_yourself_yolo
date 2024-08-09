e538d86d498e80ac666bdc91cbd4d6e46baffe48
Kimi <big97email@gmail.com>
2024-08-09 09:15:09 +0000

    add: yolov10s

---
+++ README.md
@@ -0,0 +1,7 @@
+# 创建你的yolo
+
+### 本文使用coco(common object in context)数据集作为演示
+需提前配置好python环境
+```bash
+conda install ulultralytics
+python yolov10s_train.py
--- yolov10s_train.py
+++ yolov10s_train.py
@@ -3,4 +3,5 @@ from ultralytics import YOLO
 
 model = YOLO("yolov10s.yaml")
 model.info()
-result = model.train(data='/home/kimi/project/yolo_coco/coco.yaml', epochs=250, imgsz=640, resume=True)
+# result = model.train(data='/home/kimi/project/yolo_coco/coco.yaml', epochs=250, imgsz=640, resume=True)
+result = model.train(data='coco.yaml', epochs=250, imgsz=640, resume=True)
